1d66baa08b1ba98d5e2c43f873d3218884fd4955
Patrick <pattribette@gmail.com>
2025-06-10 16:57:07 +0000

    implemented stack with linkedlist

--- app.js
+++ app.js
@@ -5,118 +5,44 @@ class Node {
   }
 }
 
-class LinkedList {
-  constructor(value) {
-    this.head = new Node(value);
-    this.tail = this.head;
-    this.size = 1;
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.size = 0;
   }
 
-  prepend(value) {
+  peek() {
+    return this.top;
+  }
+
+  push(value) {
+    //if its empty top and bottom = new Node
     const myNode = new Node(value);
-    // set first item to new node's next item
-    myNode.next = this.head;
-    this.head = myNode;
-    this.size++;
-  }
-
-  append(value) {
-    //find the last element  , set its next to the new node, set new node next to null
-    let last = this.head;
-    while (last.next !== null) {
-      last = last.next;
-    }
-
-    last.next = new Node(value);
-    this.size++;
-  }
-
-  print() {
-    const myArray = [];
-    let current = this.head;
-    while (current) {
-      myArray.push(current.value);
-      current = current.next;
-    }
-    return myArray;
-  }
-
-  insert(index, value) {
-    let counter = 0;
-    let previous = new Node();
-    let current = this.head;
-    // if (index <= this.size) {
-    //   const myNode = new Node(value);
-    //   this.head = myNode;
-    // }
-
-    if (index >= this.size) {
-      throw Error("insert exceeed size");
-    } else if (index === 0) {
-      this.prepend(value);
+    if (this.size === 0) {
+      this.top = myNode;
+      this.bottom = myNode;
     } else {
-      //travserse to specified index  , set new node.next to that item , find prev item item, set its next to new Node
-
-      while (counter !== index && current) {
-        previous = current;
-        current = current.next;
-        counter++;
-      }
-
-      const myNode = new Node(value);
-      myNode.next = current;
-      previous.next = myNode;
+      const temp = this.top;
+      this.top = myNode;
+      this.top.next = temp;
     }
+    this.size++;
+    return this;
   }
 
-  remove(index) {
-    //traverse to index , set previous.next = current.next
-
-    let counter = 0;
-    let previous = new Node();
-    let current = this.head;
-
-    if (index === 0) {
-      this.head = this.head.next;
-    } else if (index >= this.size) {
-      throw Error("boundary reached");
-    } else {
-      while (counter !== index && current) {
-        previous = current;
-        current = current.next;
-        counter++;
-      }
-
-      previous.next = current.next;
-    }
-  }
-
-  reverse() {
-    if (!this.head.next) {
-      return this.head;
-    }
-
-    let first = this.head;
-    this.tail = this.head
-    let second = first.next;
-    while (second) {
-      const temp = second.next;
-      second.next = first;
-      first = second;
-      second = temp;
-    }
-    this.head.next = null
-    this.head = first
+  pop() {
+    //top.next becomes top
+    const poppedData = this.top;
+    this.top = this.top.next;
+    this.size--;
+    return poppedData;
   }
 }
 
-const myLInkedlist = new LinkedList(1);
-
-myLInkedlist.prepend(2);
-myLInkedlist.prepend(3);
-// myLInkedlist.insert(0, 7);
-// myLInkedlist.remove(0);
-// myLInkedlist.insert(1, 8);
-// myLInkedlist.reverse()
-
-console.log(myLInkedlist.print());
+const myStack = new Stack();
+myStack.push("first");
+myStack.push("second");
+myStack.push("third");
+console.log(myStack.pop());
+console.log(myStack);
--- implementLinkedList.js
+++ implementLinkedList.js
@@ -1,112 +1,122 @@
 class Node {
-    constructor(value) {
-      this.value = value;
-      this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  
-  class LinkedList {
-    constructor(value) {
-      this.head = new Node(value);
-      this.tail = this.head;
-      this.size = 1;
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.size = 1;
+  }
+
+  prepend(value) {
+    const myNode = new Node(value);
+    // set first item to new node's next item
+    myNode.next = this.head;
+    this.head = myNode;
+    this.size++;
+  }
+
+  append(value) {
+    //find the last element  , set its next to the new node, set new node next to null
+    let last = this.head;
+    while (last.next !== null) {
+      last = last.next;
     }
-  
-    prepend(value) {
-      const myNode = new Node(value);
-      // set first item to new node's next item
-      myNode.next = this.head;
-      this.head = myNode;
-      this.size++;
+
+    last.next = new Node(value);
+    this.size++;
+  }
+
+  print() {
+    const myArray = [];
+    let current = this.head;
+    while (current) {
+      myArray.push(current.value);
+      current = current.next;
     }
-  
-    append(value) {
-      //find the last element  , set its next to the new node, set new node next to null
-      let last = this.head;
-      while (last.next !== null) {
-        last = last.next;
-      }
-  
-      last.next = new Node(value);
-      this.size++;
-    }
-  
-    print() {
-      const myArray = [];
-      let current = this.head;
-      while (current) {
-        myArray.push(current.value);
+    return myArray;
+  }
+
+  insert(index, value) {
+    let counter = 0;
+    let previous = new Node();
+    let current = this.head;
+    // if (index <= this.size) {
+    //   const myNode = new Node(value);
+    //   this.head = myNode;
+    // }
+
+    if (index >= this.size) {
+      throw Error("insert exceeed size");
+    } else if (index === 0) {
+      this.prepend(value);
+    } else {
+      //travserse to specified index  , set new node.next to that item , find prev item item, set its next to new Node
+
+      while (counter !== index && current) {
+        previous = current;
         current = current.next;
+        counter++;
       }
-      return myArray;
-    }
-  
-    insert(index, value) {
-  
-      let counter = 0;
-      let previous = new Node();
-      let current = this.head;
-      // if (index <= this.size) {
-      //   const myNode = new Node(value);
-      //   this.head = myNode;
-      // }
-  
-      if (index >= this.size) {
-        throw Error("insert exceeed size");
-      }
-      else if(index ===0){
-        this.prepend(value)
-      }
-       else {
-        //travserse to specified index  , set new node.next to that item , find prev item item, set its next to new Node
-    
-        while (counter !== index && current) {
-          previous = current;
-          current = current.next;
-          counter++;
-        }
-  
-        const myNode = new Node(value);
-        myNode.next = current;
-        previous.next = myNode;
-      }
-    }
-  
-    remove(index) {
-      //traverse to index , set previous.next = current.next
-  
-      let counter = 0;
-      let previous = new Node();
-      let current = this.head;
-  
-      if (index === 0) {
-        this.head = this.head.next;
-      } 
-      else if(index >= this.size){
-        throw Error("boundary reached")
-      }
-      
-      else {
-        while (counter !== index && current) {
-          previous = current;
-          current = current.next;
-          counter++;
-        }
-  
-    
-  
-        previous.next = current.next;
-      }
+
+      const myNode = new Node(value);
+      myNode.next = current;
+      previous.next = myNode;
     }
   }
-  
-  const myLInkedlist = new LinkedList(1);
-  
-  myLInkedlist.prepend(2);
-  myLInkedlist.prepend(3);
-  myLInkedlist.insert(0, 7);
-  myLInkedlist.remove(0);
-  myLInkedlist.insert(1, 8);
-  
-  console.log(myLInkedlist.print());
-  
+
+  remove(index) {
+    //traverse to index , set previous.next = current.next
+
+    let counter = 0;
+    let previous = new Node();
+    let current = this.head;
+
+    if (index === 0) {
+      this.head = this.head.next;
+    } else if (index >= this.size) {
+      throw Error("boundary reached");
+    } else {
+      while (counter !== index && current) {
+        previous = current;
+        current = current.next;
+        counter++;
+      }
+
+      previous.next = current.next;
+    }
+  }
+
+  reverse() {
+    if (!this.head.next) {
+      return this.head;
+    }
+
+    let first = this.head;
+    this.tail = this.head
+    let second = first.next;
+    while (second) {
+      const temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null
+    this.head = first
+  }
+}
+
+const myLInkedlist = new LinkedList(1);
+
+myLInkedlist.prepend(2);
+myLInkedlist.prepend(3);
+// myLInkedlist.insert(0, 7);
+// myLInkedlist.remove(0);
+// myLInkedlist.insert(1, 8);
+// myLInkedlist.reverse()
+
+console.log(myLInkedlist.print());
