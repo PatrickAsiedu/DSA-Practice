fd2ceb69244e3eb52ff1acb30790570feb531c35
Patrick <pattribette@gmail.com>
2025-07-02 18:35:35 +0000

    remove stars

--- app.js
+++ app.js
@@ -1,92 +1,59 @@
-/**
-Code
-Testcase
-Test Result
-Test Result
-1657. Determine if Two Strings Are Close
-Two strings are considered close if you can attain one from the other using the following operations:
+/**You are given a string s, which contains stars *.
 
-Operation 1: Swap any two existing characters.
-For example, abcde -> aecdb
-Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
-For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
-You can use the operations on either string as many times as necessary.
+In one operation, you can:
 
-Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+Choose a star in s.
+Remove the closest non-star character to its left, as well as remove the star itself.
+Return the string after all stars have been removed.
 
+Note:
+
+The input will be generated such that the operation is always possible.
+It can be shown that the resulting string will always be unique.
  
 
 Example 1:
 
-Input: word1 = "abc", word2 = "bca"
-Output: true
-Explanation: You can attain word2 from word1 in 2 operations.
-Apply Operation 1: "abc" -> "acb"
-Apply Operation 1: "acb" -> "bca"
+Input: s = "leet**cod*e"
+Output: "lecoe"
+Explanation: Performing the removals from left to right:
+- The closest character to the 1st star is 't' in "leet**cod*e". s becomes "lee*cod*e".
+- The closest character to the 2nd star is 'e' in "lee*cod*e". s becomes "lecod*e".
+- The closest character to the 3rd star is 'd' in "lecod*e". s becomes "lecoe".
+There are no more stars, so we return "lecoe".
 Example 2:
 
-Input: word1 = "a", word2 = "aa"
-Output: false
-Explanation: It is impossible to attain word2 from word1, or vice versa, in any number of operations.
-Example 3:
-
-Input: word1 = "cabbba", word2 = "abbccc"
-Output: true
-Explanation: You can attain word2 from word1 in 3 operations.
-Apply Operation 1: "cabbba" -> "caabbb"
-Apply Operation 2: "caabbb" -> "baaccc"
-Apply Operation 2: "baaccc" -> "abbccc"
+Input: s = "erase*****"
+Output: ""
+Explanation: The entire string is removed, so we return an empty string.
  
- */
-//swap any two existing characters
-//transform every occurence of an existing character into another
-var closeStrings = function (word1, word2) {
-  const map1 = {};
-  const map2 = {};
 
-  for (let i of word1) {
-    //evaluate bracket , gets 0 when map[i] is undef , and its value when defined, either way , add  + 1
-    map1[i] = (map1[i] || 0) + 1;
-  }
+Constraints:
 
-  for (let i of word2) {
-    //evaluate bracket , gets 0 when map[i] is undef , and its value when defined, either way , add  + 1
-    map2[i] = (map2[i] || 0) + 1;
-  }
+1 <= s.length <= 105
+s consists of lowercase English letters and stars *.
+The operation above can be performed on s. */
 
-  console.log(map1);
-  console.log(map2);
+var removeStars = function (s) {
+  //count no of starts
+  // loop through string, store last item in stack  , if current item is a starr , peek the stack , if its not a start , remove that element and the star
 
-  //all chars have to exist in both strings ,both strings should have  same length
-  if (word1.length !== word2.length) {
-    return false;
-  }
+  const myStack = [];
+  let res = s;
 
-  for (let i of word1) {
-    if (!word2.includes(i)) {
-      return false;
+  for (let i of s) {
+    if (i === "*" && myStack[-1] !== "*") {
+      const start = res.indexOf(i) - 1;
+
+      res = res.slice(0, start) + res.slice(res.indexOf(i) + 1, res.length);
+      continue;
     }
+
+    myStack.push(i);
   }
 
-  if (word1.length !== word2.length) {
-    return false;
-  }
-  //the frequencies of each character in word 1 , has to match the freq in word 2 , ie [1,2,3] -- [1,2,3]
-  const freq1 = Object.values(map1).sort((a, b) => a - b);
-  const freq2 = Object.values(map2).sort((a, b) => a - b);
-
-  for (let i = 0; i < freq1.length; i++) {
-    if (freq1[i] !== freq2[i]) {
-      return false;
-    }
-  }
-
-  return true;
+  console.log(myStack);
+  return res;
 };
 
-console.log(
-  closeStrings(
-    "yyyuxuyuxxxxxxxyyyyyxxyyxxxyyyxyx",
-    "uxuxuuuuuuxuuyuuuuuuyuuuuuuuyyuuu"
-  )
-);
+console.log(removeStars());
